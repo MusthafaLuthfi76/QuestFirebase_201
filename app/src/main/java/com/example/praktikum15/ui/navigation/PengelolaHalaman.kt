e5b0c783892a969502a52dc5434d1ef3fbc9fab3
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-01-12 19:30:57 +0700

    Menambahkan PengelolaHalaman.kt

---
+++ app/src/main/java/com/example/praktikum15/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.praktikum15.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
+import com.example.praktikum15.ui.view.HomeScreen
+import com.example.praktikum15.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost (
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
